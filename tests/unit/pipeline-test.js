4c3fe9613189eb1b2e35081a96b6acec56d82ab6
Lauren Tan <laurent@netflix.com>
2017-04-13 20:21:55 -0700

    Initial implementation

---
+++ tests/unit/pipeline-test.js
@@ -0,0 +1,164 @@
+import Ember from 'ember';
+import sinon from 'sinon';
+import { pipeline, step, CANCEL } from 'ember-pipeline';
+import { IS_PIPELINE_SYMBOL } from 'ember-pipeline/-symbols';
+import { module, test } from 'qunit';
+
+let sandbox;
+const {
+  Object: EmberObject,
+  RSVP: { resolve },
+  run } = Ember;
+const dummy = {
+  makeCalculationPipeline() {
+    return pipeline(this, [
+      step('step1'),
+      step('step2'),
+      step('step3')
+    ]).onCancel((cancellation) => this.handleCancel(cancellation));
+  },
+  step1(v) {
+    return v;
+  },
+  step2(v) {
+    let value = v + v;
+    if (value > 10) {
+      return CANCEL;
+    }
+    return value;
+  },
+  step3(v) {
+    return this.result = v * v;
+  },
+  handleCancel(cancellation) {
+    switch (cancellation.fnName) {
+      case 'step1':
+        this.result = `cancelled on step1 - last value: ${cancellation.result}`;
+        break;
+      case 'step2':
+        this.result = `cancelled on step2 - last value: ${cancellation.result}`;
+        break;
+      case 'step3':
+        this.result = `cancelled on step3 - last value: ${cancellation.result}`;
+        break;
+      default:
+        this.result = 'no cancel handler'
+        break;
+    }
+  },
+  calculate(...args) {
+    return this.makeCalculationPipeline().perform(...args);
+  }
+};
+
+module('Unit | Main | index', {
+  beforeEach() {
+    dummy.result = undefined;
+    sandbox = sinon.sandbox.create();
+  },
+  afterEach() {
+    sandbox.restore();
+  }
+});
+
+test('it handles happy path', function(assert) {
+  let spy = sandbox.spy(dummy, 'handleCancel');
+  assert.equal(dummy.calculate(1), 4);
+  assert.equal(dummy.calculate(2), 16);
+  assert.equal(dummy.calculate(3), 36);
+  assert.equal(dummy.calculate(4), 64);
+  assert.equal(dummy.calculate(5), 100);
+  assert.equal(spy.callCount, 0, 'should not invoke cancel handler');
+});
+
+test('it handles unhappy path', function(assert) {
+  let spy = sandbox.spy(dummy, 'step3');
+  dummy.calculate(1);
+  assert.equal(dummy.result, 4);
+  assert.equal(spy.callCount, 1);
+
+  dummy.calculate(10);
+  assert.equal(dummy.result, 'cancelled on step2 - last value: 10');
+  assert.equal(spy.callCount, 1, 'should abort chain');
+
+  dummy.calculate(1);
+  assert.equal(dummy.result, 4);
+  assert.equal(spy.callCount, 2);
+});
+
+test('#pipeline returns new pipeline instance', function(assert) {
+  let obj = {
+    foo(val) {
+      return 'foo' + val;
+    }
+  };
+  let pipelineInstance = pipeline(obj, [step('foo')]);
+  assert.ok(pipelineInstance.get(IS_PIPELINE_SYMBOL));
+  assert.equal(pipelineInstance.perform('bar'), 'foobar');
+});
+
+test('#pipeline handles steps that returns a promise', function(assert) {
+  let done = assert.async();
+  let obj = {
+    foo(val) {
+      return resolve('Foo' + val);
+    },
+    baz(val) {
+      return val + 'Baz';
+    }
+  };
+  let pipelineInstance = pipeline(obj, [
+    step('foo'),
+    step('baz')
+  ]);
+  pipelineInstance.perform('Bar').then((result) => {
+    assert.equal(result, 'FooBarBaz');
+    done();
+  });
+});
+
+test('#pipeline throws an error if context is missing or not an object', function(assert) {
+  assert.throws(() => pipeline(undefined, []));
+  assert.throws(() => pipeline(null, []));
+  assert.throws(() => pipeline([]));
+  assert.throws(() => pipeline(1, []));
+});
+
+test('#pipeline handles steps that are functions', function(assert) {
+  let obj = {
+    qux(v) {
+      return v + 'Qux';
+    }
+  };
+  let pipelineInstance = pipeline(obj, [
+    step(x => 'Foo' + x),
+    step(x => x + 'Baz'),
+    step('qux')
+  ]);
+  assert.equal(pipelineInstance.perform('Bar'), 'FooBarBazQux');
+});
+
+test('#step accepts function or string', function(assert) {
+  assert.equal(step(x => x * x).fn(2), 4);
+  assert.equal(step('foo').fnName, 'foo');
+});
+
+test('it halts reducer when the context object is destroyed', function(assert) {
+  let done = assert.async();
+  let obj = EmberObject.create({
+    foo(val) {
+      return 'foo' + val;
+    }
+  });
+  let pipelineInstance = pipeline(obj, [step('foo')]);
+  let expectedResult = {
+    '@@ember-pipeline/is-cancelled': true,
+    fnName: 'foo',
+    result: undefined
+  };
+  run(() => obj.destroy());
+  run(() => {
+    assert.deepEqual(pipelineInstance.perform('bar'), expectedResult);
+    done();
+  });
+});
---
+++ tests/unit/utils/reduce-test.js
@@ -0,0 +1,23 @@
+import reduce from 'ember-pipeline/utils/reduce';
+import { CANCEL } from 'ember-pipeline';
+import { IS_CANCELLED_SYMBOL } from 'ember-pipeline/-symbols';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | reduce');
+
+test('it mimics `Array.reduce`', function(assert) {
+  assert.equal(reduce([1, 2, 3], (acc, curr) => acc + curr, 0), 6);
+  assert.deepEqual(reduce([1, 2, 3], (acc, curr) => [...acc, curr * 2], []), [2, 4, 6]);
+});
+
+test('it short-circuits if it encounters a `CANCEL` token', function(assert) {
+  let expectedResult = {
+    [IS_CANCELLED_SYMBOL]: true,
+    result: 10, // <- last result before cancellation
+    fnName: undefined
+  };
+  let fns = [x => x + 1, () => CANCEL, x => x * x];
+  let result = reduce(fns, (acc, fn) => fn(acc), 9);
+
+  assert.deepEqual(result, expectedResult);
+});
