bd9513fbd375117dbcd1c68746248772961e54f8
Idi Eradiri <joppa27@users.noreply.github.com>
2017-05-27 12:56:01 -0400

    hopefully have write privilges to tmp

--- testem.js
+++ testem.js
@@ -14,7 +14,8 @@ module.exports = {
       "--disable-gpu",
       "--remote-debugging-port=9222",
       "--remote-debugging-address=0.0.0.0",
-      "--no-sandbox"
+      "--no-sandbox",
+      "--user-data-dir=/tmp"
     ]
   }
 };
